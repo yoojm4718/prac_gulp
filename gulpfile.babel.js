a0dc841e7ed2504e2d3a8e119c2814521ff05a22
yoojm4718 <yoojm4718@users.noreply.github.com>
2021-07-13 23:34:30 +0900

    Install & Configure browserSync, style.css -> styles.css and Change Dir of styles.css into root file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,12 @@
 import del from "del";
 import gulp from "gulp";
 import gpug from "gulp-pug";
-import ws from "gulp-webserver";
+import browserSync from "browser-sync";
 import image from "gulp-image";
-const sass = require("gulp-sass")(require("sass"));
+import ssass from "sass";
+import gsass from "gulp-sass";
+const sass = gsass(ssass);
+const reload = browserSync.reload;
 
 const routes = {
   pug: {
@@ -17,23 +20,27 @@ const routes = {
   },
   scss: {
     watch: "src/scss/**/*.scss",
-    src: "src/scss/style.scss",
-    dest: "build/css/style.css",
+    src: "src/scss/styles.scss",
+    dest: "build",
   },
 };
 
 const pug = () =>
-  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+  gulp
+    .src(routes.pug.src)
+    .pipe(gpug())
+    .pipe(gulp.dest(routes.pug.dest))
+    .pipe(reload({ stream: true }));
 
 const clean = () => del(["build"]);
 
-const webserver = () =>
-  gulp.src("build").pipe(ws({ livereload: true, open: true }));
-
-const watch = () => {
-  gulp.watch(routes.pug.watch, pug);
-  gulp.watch(routes.img.src, img);
-  gulp.watch(routes.scss.watch, styles);
+const devServer = () => {
+  browserSync.init({
+    server: {
+      baseDir: "build",
+    },
+  });
+  watch();
 };
 
 const img = () =>
@@ -43,12 +50,19 @@ const styles = () =>
   gulp
     .src(routes.scss.src)
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest(routes.scss.dest));
+    .pipe(gulp.dest(routes.scss.dest))
+    .pipe(reload({ stream: true }));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.watch, styles);
+};
 
 const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug, styles]);
 
-const postDev = gulp.series([webserver, watch]);
+const postDev = gulp.series([devServer]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
